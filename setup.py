6b66fda9050b25928d52f2eff2e3c0c3f89cffce
Герой Интернет <ustim2k7@gmail.comgit config --global user.email ustim2k7@gmail.comgit config --global user.email ustim2k7@gmail.com>
2020-09-27 00:50:54 +0300

    adding files

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='blogengine',
+    version='1.0.0',
+    packages=['blog', 'blog.migrations', 'blogengine'],
+    url='https://github.com/Marshak2k7/blog',
+    license='MIT',
+    author='Ustimenko Timur',
+    author_email='ustim2k7@gmail.com',
+    description=' simple project to practice django'
+)
